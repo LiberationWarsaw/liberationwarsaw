bf4380297e3a2a0a60f48a22d81abed74dcc061a
Mary Kate Fain <mkfain1@gmail.com>
2018-01-02 17:10:19 -0500

    home page event templates

--- core/views.py
+++ core/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.views.generic.base import TemplateView
 from core.models import Event, Tag, Level
 from django.views.generic import ListView
+from django.utils import timezone
+
 
 
 class HomePageView(TemplateView):
@@ -10,10 +12,19 @@ class HomePageView(TemplateView):
     """
     template_name = 'core/index.html'
 
+    def get_context_data(self, **kwargs):
+        context = super(HomePageView, self).get_context_data(**kwargs)
+        context['upcoming_events'] = Event.objects.filter(
+            start_time__gte=timezone.now()
+        ).order_by('-start_time')[:4]
+        return context
+
+
 class AboutPageView(TemplateView):
     """
     """
     template_name = 'core/about.html'
+
 
 class EventsListView(ListView):
     """
@@ -21,20 +32,24 @@ class EventsListView(ListView):
     template_name = 'core/event-list.html'
     model = Event
 
+
 class ContactPageView(TemplateView):
     """
     """
     template_name = 'core/contact.html'
+
 
 class PledgePageView(TemplateView):
     """
     """
     template_name = 'core/pledge.html'
 
+
 class RescuePageView(TemplateView):
     """
     """
-    template_name = 'core//rescue.html'
+    template_name = 'core/rescue.html'
+
 
 class SavePageView(TemplateView):
     """
