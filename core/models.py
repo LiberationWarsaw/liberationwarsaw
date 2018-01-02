cf42cf667cfa3f7b7492dde7b4daf32f6f1eb54b
Mary Kate Fain <mkfain1@gmail.com>
2018-01-02 14:17:44 -0500

    event models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+class Event(models.Model):
+    """
+    A Liberation Philly event.
+    """
+    title = models.CharField(max_length=255)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    description = models.TextField()
+    link = models.URLField(max_length=255)
+    tags = models.ManyToManyField('Tag')
+    level = models.ManyToManyField('Level')
+    contact = models.EmailField(max_length=255)
+
+
+    def __str__(self):
+        return self.title
+
+class Level(models.Model):
+    """
+    The risk levels for a particular event.
+    """
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+class Tag(models.Model):
+    """
+    The types of action present at a particular event.
+    """
+    title = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.title
