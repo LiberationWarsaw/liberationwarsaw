6d73956cb6cd91eaa042ac9274fbdd8b3f6b8172
Mary Kate Fain <mkfain1@gmail.com>
2018-01-02 14:32:39 -0500

    import models to admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Event, Level, Tag
 
-# Register your models here.
+admin.site.register(Event)
+admin.site.register(Level)
+admin.site.register(Tag)
