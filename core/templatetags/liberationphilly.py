bf615374c5ae9a6527bf470f770838127032049f
Mary Kate Fain <mkfain1@gmail.com>
2018-01-02 20:04:06 -0500

    add line breaks on date/time

--- core/templatetags/liberationphilly.py
+++ core/templatetags/liberationphilly.py
@@ -1,18 +1,25 @@
 from django import template
+from django.utils.safestring import mark_safe
 
 register = template.Library()
 
 @register.simple_tag
-def format_dates(start, end):
+def format_dates(start, end, multiline=False):
+    if multiline == "true":
+        multiline = True
+
     if start.date() == end.date():
         date_format = "%A %b %d"
         time_format = "%I:%M%p"
         # Tuesday Jan 2, 1:00PM–4:00PM
-        return "{date}, {start_time}–{end_time}".format(
+        template_string = "{date}, {start_time} – {end_time}"
+        if multiline:
+            template_string = "{date}<br>{start_time} – {end_time}"
+        return mark_safe(template_string.format(
             date=start.date().strftime(date_format),
             start_time=start.time().strftime(time_format),
             end_time=end.time().strftime(time_format)
-        )
+        ))
 
     elif (end.date() - start.date()).seconds <= 24*60*60:
         # If less than 24 hours difference:
@@ -20,19 +27,25 @@ def format_dates(start, end):
         time_format = "%I:%M%p"
 
         # Tuesday Jan 2, 10:00PM – Wednesday Jan 3, 11:00PM
-        return "{start_date}, {start_time} – {end_date}, {end_time}".format(
+        template_string = "{start_date}, {start_time} – {end_date}, {end_time}"
+        if multiline:
+            template_string = "{start_date}, {start_time}<br>{end_date}, {end_time}"
+        return mark_safe(template_string.format(
             start_date=start.date().strftime(date_format),
             start_time=start.time().strftime(time_format),
             end_date=end.date().strftime(date_format),
             end_time=end.time().strftime(time_format),
-        )
+        ))
 
     else:
         # If more than 24 hours difference:
         date_format = "%A %b %d, %Y"
 
         # Tuesday Jan 2, 2018 – Saturday Jan 6, 2018
-        return "{start_date} – {end_date}".format(
+        template_string = "{start_date} – {end_date}"
+        if multiline:
+            template_string = "{start_date}<br>{end_date}"
+        return mark_safe(template_string.format(
             start_date=start.date().strftime(date_format),
             end_date=end.date().strftime(date_format),
-        )
+        ))
