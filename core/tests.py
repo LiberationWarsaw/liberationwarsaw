ba6477c959b3541929217c78143540a9356e074d
Mary Kate Fain <mkfain1@gmail.com>
2018-01-05 10:09:14 -0500

    started tests

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,10 @@
-from django.test import TestCase
+import datetime
 
-# Create your tests here.
+from django.test import TestCase
+from django.utils import timezone
+
+# A test to make sure the index page is returning exactly 4 events, all in the future, in order starting with soonest.
+
+
+
+# A test to make sure the events list is returning all future events in order starting with the soonest
