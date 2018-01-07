e74481c4e073887993c1236a09aeb94000963329
Mary Kate Fain <mkfain1@gmail.com>
2018-01-06 19:11:31 -0500

    improve filter functionality on events list

--- core/static/main.js
+++ core/static/main.js
@@ -1,5 +1,6 @@
 function filterAllEvents() {
   var unselected_events = document.querySelectorAll('.event-page-item');
+
   for (i = 0; i < unselected_events.length; i++) {
     unselected_events[i].classList.add('inactive');
   }
@@ -7,15 +8,43 @@ function filterAllEvents() {
 
 function clearFilters() {
   var unselected_events = document.querySelectorAll('.event-page-item');
+
   for (i = 0; i < unselected_events.length; i++) {
     unselected_events[i].classList.remove('inactive');
   }
+
+  var buttons = document.querySelectorAll('.filter.button');
+
+  for (i = 0; i < buttons.length; i++) {
+    buttons[i].classList.remove('active');
+  }
 }
 
-function filterEvents(query) {
-  filterAllEvents();
+function filterEvents(query, button) {
   var selected_events = document.querySelectorAll(query);
+
+  function toggleEvents() {
     for (i = 0; i < selected_events.length; i++) {
       selected_events[i].classList.toggle('inactive');
     }
   }
+
+  function toggleButton() {
+    console.log(button);
+    button.classList.toggle('active');
+    console.log('changed');
+  }
+
+// If button is inactive:
+  if (button.classList.contains('active') == true) {
+    clearFilters();
+    console.log('True');
+// If button is already active:
+  } else {
+    clearFilters();
+    filterAllEvents();
+    toggleEvents();
+    toggleButton();
+    console.log('False');
+  }
+}
